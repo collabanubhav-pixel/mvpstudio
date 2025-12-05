03e4270f9c57c01e714f8592cd9724343337255d
collabanubhav-pixel <collab.anubhav@gmail.com>
2025-12-05 12:03:42 +0530

    Updated Navbar.tsx

--- components/marketing/Navbar.tsx
+++ components/marketing/Navbar.tsx
@@ -18,9 +18,9 @@ export function Navbar() {
               <Image
                 src="/mvpstudiologo.png"
                 alt="MVP Studio"
-                width={120}
-                height={40}
-                className="h-8 w-auto"
+                width={150}
+                height={50}
+                className="h-10 w-auto"
               />
             </Link>
           </div>
